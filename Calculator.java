d5790aadd63a1cc10156017e09c007c1962de3f3
Андрей <werewolf-87@mail.ru>
2022-02-08 14:23:25 +0700

    Консольный калькулятор. Версия 3.0

--- Calculator.java
+++ Calculator.java
@@ -4,46 +4,51 @@ import java.util.Scanner; // подключаем класс —канер
 
 
 public class Calculator {
+public static Scanner scanner = new Scanner(System.in); // создаем объект сканер дл€ чтени€ из консоли
+public static boolean exit = false; // задаем переменную "выход", с присвоением значени€ "ложь"
+
 
    public static void main(String[] args) {
 
-Scanner scanner = new Scanner(System.in); // создаем объект сканер дл€ чтени€ из консоли
-	boolean exit = false; // задаем переменную "выход", с присвоением значени€ "ложь"
 System.out.println("¬ведите число: ");
-	double x = scanner.nextDouble(); // задаем первое число, с присвоением значени€ из консоли
-	double result = x; // задаем переменную "результат", с присвоением значени€ первого числа
-System.out.println("¬ведите оператор (+, -, *, /): ");
-char operator = scanner.next().charAt(0); // задаем оператор, с присвоением значени€ из консоли
+double result = scanner.nextDouble(); // задаем переменную "результат", с присвоением значени€ из консоли
+
+System.out.println("¬ведите оператор (+, -, *, /) или \"=\" дл€ получени€ результата: ");
+
 
 while(!exit) {
 
+String operator = scanner.nextLine(); // задаем переменную "оператор", с присвоением значени€ из консоли
 
-System.out.println("¬ведите число или введите \"=\" дл€ получени€ результата: ");
-if (scanner.hasNextDouble()) { // провер€ем €вл€ютс€ ли введенные данные числом
+	if (operator.equals("+")) {
+System.out.println("¬ведите число: ");
+double y = scanner.nextDouble();
+result = result + y;
+System.out.println("¬ведите оператор (+, -, *, /) или \"=\" дл€ получени€ результата: ");                  
+	} else if (operator.equals("-")) {
+System.out.println("¬ведите число: ");
+double y = scanner.nextDouble();
+result = result - y;
+System.out.println("¬ведите оператор (+, -, *, /) или \"=\" дл€ получени€ результата: ");
 
-                double y = scanner.nextDouble();
-                switch(operator) {
-         case '+': result = result + y;
-            break;
-         case '-': result = result - y;
-            break;
-         case '*': result = result * y;
-            break;
-         case '/': result = result / y;
-            break;
-         default:  System.out.printf("ќшибка. ¬веден некорректный оператор!");
-            return;
-      }
-System.out.println("¬ведите оператор (+, -, *, /): ");
-operator = scanner.next().charAt(0);
+	} else if (operator.equals("*")) {
+System.out.println("¬ведите число: ");
+double y = scanner.nextDouble();
+result = result * y;
+System.out.println("¬ведите оператор (+, -, *, /) или \"=\" дл€ получени€ результата: ");
 
-    } else if (scanner.hasNextLine()) { // провер€ем €вл€ютс€ си введенные данные символом
-                String line = scanner.nextLine();
-                if (line.equalsIgnoreCase("=")) {
-                    exit = true;
-                }
-            }
+	} else if (operator.equals("/")) {
+System.out.println("¬ведите число: ");
+double y = scanner.nextDouble();
+result = result / y;
+System.out.println("¬ведите оператор (+, -, *, /) или \"=\" дл€ получени€ результата: ");
+
+	} else if (operator.equals("=")) {
+ exit = true;
+}            
         } // при помощи цикла while производим операции над введенными числами
-System.out.println("–езультат равен: " + result); // выводим полученный результат 
+
+System.out.println("–езультат равен: " + result); // выводим полученный результат
+
    }
 }
